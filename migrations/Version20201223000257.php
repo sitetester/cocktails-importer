44cb2da9db3207694b8443efb37ae5fe4a62bbd0
sitetester <sitetester2010@gmail.com>
2020-12-23 02:05:39 +0200

    bug fixes

--- migrations/Version20201223000257.php
+++ migrations/Version20201223000257.php
@@ -12,20 +12,19 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201223000257 extends AbstractMigration
 {
-    public function getDescription() : string
-    {
-        return '';
-    }
-
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)');
-        $this->addSql('CREATE TABLE drinks (id INTEGER NOT NULL, category_id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, glass VARCHAR(255) NOT NULL, alcoholic VARCHAR(255) NOT NULL, thumbnail VARCHAR(255) NOT NULL, instructions VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql(
+            'CREATE TABLE category (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)'
+        );
+        $this->addSql(
+            'CREATE TABLE drinks (id INTEGER NOT NULL, category_id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, glass VARCHAR(255) NOT NULL, alcoholic VARCHAR(255) NOT NULL, thumbnail VARCHAR(255) NOT NULL, instructions VARCHAR(255) NOT NULL, PRIMARY KEY(id))'
+        );
         $this->addSql('CREATE INDEX IDX_EAD7930912469DE2 ON drinks (category_id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE category');
